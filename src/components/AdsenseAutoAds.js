dbb80cb075047ef99ff237e57df2502aba1ffb6e
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-24 22:49:59 +0100

    Create AdsenseAutoAds.js

---
+++ src/components/AdsenseAutoAds.js
@@ -0,0 +1,20 @@
+// components/AdsenseAutoAds.js
+import React, { useEffect } from 'react';
+
+const AdsenseAutoAds = () => {
+  useEffect(() => {
+    // Prüfen, ob das AdSense-Skript bereits geladen wurde
+    const isAdsenseLoaded = window.adsbygoogle && window.adsbygoogle.loaded;
+    if (!isAdsenseLoaded) {
+      const adsenseScript = document.createElement('script');
+      adsenseScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+      adsenseScript.async = true;
+      adsenseScript.setAttribute('data-ad-client', process.env.NEXT_PUBLIC_ADSENSE_ID);
+      document.body.appendChild(adsenseScript);
+    }
+  }, []);
+
+  return null; // Da Auto-Anzeigen keine spezifischen Elemente benötigen, geben wir null zurück
+};
+
+export default AdsenseAutoAds;
