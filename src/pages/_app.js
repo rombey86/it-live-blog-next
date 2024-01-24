3216fc763291d8af9082ac21e48fa75ca92bac8d
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-25 00:14:59 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,60 +1,67 @@
 import NextApp from 'next/app';
+import { ApolloProvider } from '@apollo/client';
 
+// Import your hooks and lib functions
 import { SiteContext, useSiteContext } from 'hooks/use-site';
 import { SearchProvider } from 'hooks/use-search';
-
+import { useApollo } from '../lib/apollo-client';
 import { getSiteMetadata } from 'lib/site';
 import { getRecentPosts } from 'lib/posts';
 import { getCategories } from 'lib/categories';
-import NextNProgress from 'nextjs-progressbar';
 import { getAllMenus } from 'lib/menus';
+
+// Import your components and styles
+import NextNProgress from 'nextjs-progressbar';
 import AdsenseAutoAds from '../components/AdsenseAutoAds';
+import FooterMenu from '../components/FooterMenu';
 import 'styles/globals.scss';
 import 'styles/wordpress.scss';
 import variables from 'styles/_variables.module.scss';
-// pages/_app.js oder Ihre Layout-Komponente
-import FooterMenu from '../components/FooterMenu';
 
-function App({ Component, pageProps = {}, metadata, recentPosts, categories, menus }) {
-  const site = useSiteContext({
-    metadata,
-    recentPosts,
-    categories,
-    menus,
-  });
+function App({ Component, pageProps }) {
+  // Initialize Apollo Client with the initial state
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  // Use the site context hook to get the necessary site data
+  const site = useSiteContext(pageProps);
 
   return (
-    <SiteContext.Provider value={site}>
-      <SearchProvider>
-        <NextNProgress height={4} color={variables.progressbarColor} />
-        <Component {...pageProps} />
-        <FooterMenu />
-        <AdsenseAutoAds />
-      </SearchProvider>
-    </SiteContext.Provider>
+    // Wrap your application with ApolloProvider and pass the client instance
+    <ApolloProvider client={apolloClient}>
+      <SiteContext.Provider value={site}>
+        <SearchProvider>
+          <NextNProgress height={4} color={variables.progressbarColor} />
+          <Component {...pageProps} />
+          <FooterMenu />
+          <AdsenseAutoAds />
+        </SearchProvider>
+      </SiteContext.Provider>
+    </ApolloProvider>
   );
 }
 
+// Fetch global data for every page
 App.getInitialProps = async function (appContext) {
   const appProps = await NextApp.getInitialProps(appContext);
 
+  // Fetch data from your lib functions
+  const metadata = await getSiteMetadata();
   const { posts: recentPosts } = await getRecentPosts({
     count: 5,
     queryIncludes: 'index',
   });
-
-  const { categories } = await getCategories({
-    count: 5,
-  });
-
+  const { categories } = await getCategories({ count: 5 });
   const { menus = [] } = await getAllMenus();
 
+  // Return the data as props to be used in the site context
   return {
     ...appProps,
-    metadata: await getSiteMetadata(),
-    recentPosts,
-    categories,
-    menus,
+    pageProps: {
+      metadata,
+      recentPosts,
+      categories,
+      menus,
+    },
   };
 };
 
