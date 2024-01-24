cfb67c225357ac714222b4aede0f0d23f9b0ff37
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-24 23:31:55 +0100

    Update apollo-client.js

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { removeLastTrailingSlash } from 'lib/util';
 
@@ -6,41 +5,23 @@ let apolloClient;
 
 function createApolloClient() {
   return new ApolloClient({
-    ssrMode: typeof window === 'undefined', // Aktivieren Sie ssrMode, wenn die Ausführung auf dem Server stattfindet
+    ssrMode: typeof window === 'undefined',
     link: new HttpLink({
       uri: removeLastTrailingSlash(process.env.WORDPRESS_GRAPHQL_ENDPOINT),
     }),
-    cache: new InMemoryCache({
-      typePolicies: {
-        RootQuery: {
-          queryType: true,
-        },
-        RootMutation: {
-          mutationType: true,
-        },
-      },
-    }),
+    cache: new InMemoryCache(),
   });
 }
 
-export function initializeApollo(initialState = null) {
+export function getApolloClient(initialState = null) {
   const _apolloClient = apolloClient ?? createApolloClient();
 
-  // Wenn Ihre Seite initialProps oder getStaticProps oder getServerSideProps verwendet
   if (initialState) {
-    // Datenwiederherstellung aus dem Cache
     _apolloClient.cache.restore(initialState);
   }
 
-  // Für SSG und SSR immer einen neuen Apollo Client erstellen
   if (typeof window === 'undefined') return _apolloClient;
-  // Einen Apollo Client einmal im Client erstellen
   if (!apolloClient) apolloClient = _apolloClient;
 
   return _apolloClient;
-}
-
-export function useApollo(initialState) {
-  const store = useMemo(() => initializeApollo(initialState), [initialState]);
-  return store;
 }
