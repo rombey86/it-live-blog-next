964aa395c151dbece86ab5ca0a01ed614af99f66
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-24 23:41:40 +0100

    Update pages.js

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -21,7 +21,7 @@ export function pagePathBySlug(slug) {
  */
 
 export async function getPageByUri(uri) {
-  const apolloClient = getApolloClient();
+  const apolloClient = useApollo(initialState);
   const apiHost = new URL(process.env.WORDPRESS_GRAPHQL_ENDPOINT).host;
 
   let pageData;
@@ -116,7 +116,7 @@ const allPagesIncludesTypes = {
 export async function getAllPages(options = {}) {
   const { queryIncludes = 'index' } = options;
 
-  const apolloClient = getApolloClient();
+  const apolloClient = useApollo(initialState);
 
   const data = await apolloClient.query({
     query: allPagesIncludesTypes[queryIncludes],
