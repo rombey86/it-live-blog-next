3c04ac8d65f176fc465bd919ed581aff9e3f91c4
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-25 00:21:06 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,9 @@
-import NextApp from 'next/app';
 import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apollo-client';
 
 // Import your hooks and lib functions
 import { SiteContext, useSiteContext } from 'hooks/use-site';
 import { SearchProvider } from 'hooks/use-search';
-import { useApollo } from '../lib/apollo-client';
-import { getSiteMetadata } from 'lib/site';
-import { getRecentPosts } from 'lib/posts';
-import { getCategories } from 'lib/categories';
-import { getAllMenus } from 'lib/menus';
 
 // Import your components and styles
 import NextNProgress from 'nextjs-progressbar';
@@ -19,14 +14,12 @@ import 'styles/wordpress.scss';
 import variables from 'styles/_variables.module.scss';
 
 function App({ Component, pageProps }) {
-  // Initialize Apollo Client with the initial state
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   // Use the site context hook to get the necessary site data
   const site = useSiteContext(pageProps);
 
   return (
-    // Wrap your application with ApolloProvider and pass the client instance
     <ApolloProvider client={apolloClient}>
       <SiteContext.Provider value={site}>
         <SearchProvider>
@@ -39,30 +32,5 @@ function App({ Component, pageProps }) {
     </ApolloProvider>
   );
 }
-
-// Fetch global data for every page
-App.getInitialProps = async function (appContext) {
-  const appProps = await NextApp.getInitialProps(appContext);
-
-  // Fetch data from your lib functions
-  const metadata = await getSiteMetadata();
-  const { posts: recentPosts } = await getRecentPosts({
-    count: 5,
-    queryIncludes: 'index',
-  });
-  const { categories } = await getCategories({ count: 5 });
-  const { menus = [] } = await getAllMenus();
-
-  // Return the data as props to be used in the site context
-  return {
-    ...appProps,
-    pageProps: {
-      metadata,
-      recentPosts,
-      categories,
-      menus,
-    },
-  };
-};
 
 export default App;
