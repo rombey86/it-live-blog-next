7dcc2e62604b57a6b3efb2eb9e22dffdf1ddb73f
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-24 23:35:30 +0100

    Update posts.js

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,4 +1,4 @@
-import { getApolloClient } from 'lib/apollo-client';
+import { initializeApollo } from 'lib/apollo-client';
 
 import { updateUserAvatar } from 'lib/users';
 import { sortObjectsByDate } from 'lib/datetime';
