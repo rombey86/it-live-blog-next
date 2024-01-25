92ae217f431a18c9f83158e348cd000191ad1a35
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-25 02:29:49 +0100

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,20 @@
-export { default } from './Footer';
+// src/components/Footer/index.js
+import React from 'react';
+import useSite from 'hooks/use-site';
+import FooterMenu from './FooterMenu';
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  const { metadata: { title } = {} } = useSite();
+
+  return (
+    <footer className={styles.footer}>
+      <FooterMenu />
+      <div className={styles.footerLegal}>
+        <p>&copy; {new Date().getFullYear()} {title}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
