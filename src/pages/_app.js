116224c8cdb3c7ba6ca78af789db5ff1b51f0c01
rombey86 <62323956+rombey86@users.noreply.github.com>
2024-01-25 20:58:48 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,22 +26,23 @@ function App({ Component, pageProps }) {
   const site = useSiteContext(pageProps);
 
   return (
-    // Wrap your application with ApolloProvider and pass the client instance
     <ApolloProvider client={apolloClient}>
       <SiteContext.Provider value={site}>
         <SearchProvider>
           <Head>
             {/* Matomo Tag Manager */}
-            <script dangerouslySetInnerHTML={{
-              __html: `
-                var _mtm = window._mtm = window._mtm || [];
-                _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-                (function() {
-                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                  g.async=true; g.src='https://matomo.it-live-blog.com/js/container_U7VVtRw5.js'; s.parentNode.insertBefore(g,s);
-                })();
-              `,
-            }} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  var _mtm = window._mtm = window._mtm || [];
+                  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+                  (function() {
+                    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+                    g.async = true; g.src = 'https://matomo.it-live-blog.com/js/container_U7VVtRw5.js'; s.parentNode.insertBefore(g, s);
+                  })();
+                `,
+              }}
+            />
           </Head>
           <NextNProgress height={4} color={variables.progressbarColor} />
           <Component {...pageProps} />
