c6426333f0ca1555561c7d52b72d0df12a23cc04
rombey86 <rombey86@googlemail.com>
2024-01-26 16:38:33 +0100

    Revert "Vor Rendern"
    This reverts commit 718bf46a85831d5bf729c9f77dd0d3ac95b2426a.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
+import NextApp from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import Head from 'next/head';
-import { useApollo } from '../lib/apollo-client';
 
-import { SiteContext } from 'hooks/use-site';
+// Import your hooks and lib functions
+import { SiteContext, useSiteContext } from 'hooks/use-site';
 import { SearchProvider } from 'hooks/use-search';
+import { useApollo } from '../lib/apollo-client';
+import { getSiteMetadata } from 'lib/site';
+import { getRecentPosts } from 'lib/posts';
+import { getCategories } from 'lib/categories';
+import { getAllMenus } from 'lib/menus';
 
+// Import your components and styles
 import NextNProgress from 'nextjs-progressbar';
 import AdsenseAutoAds from '../components/AdsenseAutoAds';
 import 'styles/globals.scss';
@@ -12,11 +19,15 @@ import 'styles/wordpress.scss';
 import variables from 'styles/_variables.module.scss';
 
 function App({ Component, pageProps }) {
+  // Initialize Apollo Client with the initial state
   const apolloClient = useApollo(pageProps.initialApolloState);
+
+  // Use the site context hook to get the necessary site data
+  const site = useSiteContext(pageProps);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <SiteContext.Provider value={pageProps}>
+      <SiteContext.Provider value={site}>
         <SearchProvider>
           <Head>
             {/* Matomo Tag Manager */}
@@ -41,5 +52,30 @@ function App({ Component, pageProps }) {
     </ApolloProvider>
   );
 }
+
+// Fetch global data for every page
+App.getInitialProps = async function (appContext) {
+  const appProps = await NextApp.getInitialProps(appContext);
+
+  // Fetch data from your lib functions
+  const metadata = await getSiteMetadata();
+  const { posts: recentPosts } = await getRecentPosts({
+    count: 5,
+    queryIncludes: 'index',
+  });
+  const { categories } = await getCategories({ count: 5 });
+  const { menus = [] } = await getAllMenus();
+
+  // Return the data as props to be used in the site context
+  return {
+    ...appProps,
+    pageProps: {
+      metadata,
+      recentPosts,
+      categories,
+      menus,
+    },
+  };
+};
 
 export default App;
