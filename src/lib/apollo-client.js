ca639920af36706361073a47d3c2c2fed37dad53
Tobias Rombey <62323956+rombey86@users.noreply.github.com>
2024-03-13 14:17:11 +0100

    Update apollo-client.js

--- src/lib/apollo-client.js
+++ src/lib/apollo-client.js
@@ -9,9 +9,7 @@ function createApolloClient() {
   const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors)
       graphQLErrors.forEach(({ message, locations, path }) =>
-        console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-        ),
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
       );
     if (networkError) console.log(`[Network error]: ${networkError}`);
   });
